e4458aa7b564aa2bfa31915a71f497592ff135cd
caballero33 <josephalcerro33@gmail.com>
2024-03-14 20:05:36 -0600

    Pantalla_Login

--- lib/pantallas/login_page.dart
+++ lib/pantallas/login_page.dart
@@ -1,10 +1,90 @@
-
 import 'package:flutter/material.dart';
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+import 'registro_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                // Aqui falta la logica para verificar el inicio de sesion
+
+                String email = _emailController.text;
+                String password = _passwordController.text;
+
+                print('Email: $email');
+                print('Password: $password');
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                    fontSize: 18, color: Colors.blueAccent), // Tamaño del texto
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RegistroPage(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(foregroundColor: Colors.white),
+              child: Text(
+                'Registrarse',
+                style: TextStyle(color: Colors.blueAccent), // Color del texto
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: LoginPage(),
+  ));
 }
--- lib/pantallas/registro_page.dart
+++ lib/pantallas/registro_page.dart
@@ -1,7 +1,9 @@
-
 import 'package:flutter/material.dart';
+
 class RegistroPage extends StatelessWidget {
-   RegistroPage({super.key,});
+  RegistroPage({
+    super.key,
+  });
 
   final TextEditingController controllerNombre = TextEditingController();
   final TextEditingController controllerCorreo = TextEditingController();
@@ -12,121 +14,144 @@ class RegistroPage extends StatelessWidget {
   final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Center(child: Text('Registro')),
-      ),
-      body:  SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Card(
-          elevation: 10,
-          child: Form(
-            key: formkey, //Llave para el formulario
-            child: Column(
-              children: [
-              const Icon(Icons.person, size: 100, color: Colors.blue,),
-              InputItem(
-                controller: controllerNombre,labelText: "Nombre",
-                hintText: "Escribe tu nombre",
-                ),
-              const Padding(padding:  EdgeInsets.all(10)),
-              InputItem(
-               controller: controllerCorreo,labelText:"Correo"
-              ,hintText: "Escribe tu correo electrónico",
-              ),
-              const Padding(padding:  EdgeInsets.all(10)),
-              InputItem(controller: controllertelefono, labelText: "Telefono",
-              hintText: "Escribe tu telefono",),
-              const Padding(padding:  EdgeInsets.all(10)),
-              InputItem(
-              controller: controllerContrasenia, labelText: "Contraseña",
-              hintText: "Escribe tu contraseña",
-              ),
-              const Padding(padding:  EdgeInsets.all(10)),
-              InputItem(
-                controller: controllerContrasenia2, labelText: "Repetir contraseña",
-                hintText: "Confirma tu contraseña",
-              
-                ),
-                const Padding(padding:  EdgeInsets.all(10)),
-                ElevatedButton(
-                  onPressed: (){
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(
+              child: Text(
+            'Registro',
+            style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
+                fontWeight: FontWeight.normal),
+          )),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20),
+          child: Card(
+            elevation: 10,
+            child: Form(
+              key: formkey, //Llave para el formulario
+              child: Column(
+                children: [
+                  const Icon(
+                    Icons.person,
+                    size: 100,
+                    color: Colors.blue,
+                  ),
+                  InputItem(
+                    controller: controllerNombre,
+                    labelText: "Nombre",
+                    hintText: "Escribe tu nombre",
+                  ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  InputItem(
+                    controller: controllerCorreo,
+                    labelText: "Correo",
+                    hintText: "Escribe tu correo electrónico",
+                  ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  InputItem(
+                    controller: controllertelefono,
+                    labelText: "Telefono",
+                    hintText: "Escribe tu telefono",
+                  ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  InputItem(
+                    controller: controllerContrasenia,
+                    labelText: "Contraseña",
+                    hintText: "Escribe tu contraseña",
+                  ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  InputItem(
+                    controller: controllerContrasenia2,
+                    labelText: "Repetir contraseña",
+                    hintText: "Confirma tu contraseña",
+                  ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  ElevatedButton(
+                    onPressed: () {
+                      final nombre = controllerNombre.text;
+                      final correo = controllerCorreo.text;
+                      final telefeno = controllertelefono.text;
+                      final contrasenia = controllerContrasenia.text;
+                      final contrasenia2 = controllerContrasenia2.text;
 
-                    final nombre= controllerNombre.text;
-                    final correo= controllerCorreo.text;
-                    final telefeno = controllertelefono.text;
-                    final contrasenia= controllerContrasenia.text;
-                    final contrasenia2= controllerContrasenia2.text;
-                    
-
-                    if(nombre.isEmpty){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("El nombre no puede estar vacío"))
-                      );
-                      return;
-                    }
-
-                    if(correo.isEmpty){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("El correo no puede estar vacío"))
-                      );
-                      return;
-                    }
-
-                   if(telefeno.isEmpty){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("El telefono no puede estar vacío"))
-                      );
-                      return;
-                    }
-                    if(contrasenia.isEmpty){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("La contraseña no puede estar vacía"))
-                      );
-                      return;
-                    }
-
-                    if(contrasenia2.isEmpty){
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("La contraseña no puede estar vacía"))
-                      );
-                      return;
-                    }
-                      if(contrasenia == contrasenia2){
-                      print("Nombre: ${controllerNombre.text}");
-                      print("Correo: ${controllerCorreo.text}");
-                      print("Contraseña: ${controllerContrasenia.text}");
-                      print("Contraseña2: ${controllerContrasenia2.text}");
-                      return;
-                      }
-                      else{
+                      if (nombre.isEmpty) {
                         ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text("Las contraseñas no coinciden"))
-                        );
+                            const SnackBar(
+                                content:
+                                    Text("El nombre no puede estar vacío")));
                         return;
                       }
-                  },
-                   child: const Text("    Registrate   "),
-                   ),
-                const Padding(padding:  EdgeInsets.all(5)),
+
+                      if (correo.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content:
+                                    Text("El correo no puede estar vacío")));
+                        return;
+                      }
+
+                      if (telefeno.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content:
+                                    Text("El telefono no puede estar vacío")));
+                        return;
+                      }
+                      if (contrasenia.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    "La contraseña no puede estar vacía")));
+                        return;
+                      }
+
+                      if (contrasenia2.isEmpty) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    "La contraseña no puede estar vacía")));
+                        return;
+                      }
+                      if (contrasenia == contrasenia2) {
+                        print("Nombre: ${controllerNombre.text}");
+                        print("Correo: ${controllerCorreo.text}");
+                        print("Contraseña: ${controllerContrasenia.text}");
+                        print("Contraseña2: ${controllerContrasenia2.text}");
+                        return;
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text("Las contraseñas no coinciden")));
+                        return;
+                      }
+                    },
+                    child: const Text("    Registrate   "),
+                  ),
+                  const Padding(padding: EdgeInsets.all(5)),
                 ],
+              ),
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
 
 class InputItem extends StatefulWidget {
-  const InputItem({super.key, required this.controller, this.labelText, this.hintText, this.validator});
+  const InputItem(
+      {super.key,
+      required this.controller,
+      this.labelText,
+      this.hintText,
+      this.validator});
 
   final TextEditingController controller;
   final String? labelText;
   final String? hintText;
 
   final String? Function(String?)? validator;
-  
+
   @override
   State<InputItem> createState() => _InputItemState();
 }
@@ -137,10 +162,10 @@ class _InputItemState extends State<InputItem> {
     return TextFormField(
       validator: widget.validator,
       controller: widget.controller,
-      decoration:  InputDecoration(
-      labelText: widget.labelText ,
-      hintText: widget.hintText,
-      border: const OutlineInputBorder(),
+      decoration: InputDecoration(
+        labelText: widget.labelText,
+        hintText: widget.hintText,
+        border: const OutlineInputBorder(),
       ),
     );
   }
