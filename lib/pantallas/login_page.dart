d2d445ab1f3e7eca17fdbdc2af67fc95394b3246
caballero33 <josephalcerro33@gmail.com>
2024-03-16 21:54:26 -0600

    Version_final

--- lib/pantallas/login_page.dart
+++ lib/pantallas/login_page.dart
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
 import 'package:ap_autentication/pantallas/inicio_page.dart';
 import 'package:ap_autentication/routes.dart';
 import 'package:ap_autentication/widgets/Imput_widget.dart';
 import 'package:flutter/material.dart';
-=======
-import 'package:flutter/material.dart';
-import 'registro_page.dart';
-import 'InicioPage.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -16,32 +11,14 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
-
-  // Simulación de credenciales almacenadas
-  String Email = 'joseph.alcerro@unah.hn';
-  String Password = '20222000391';
->>>>>>> 79b1c0a2b8fedfc06fe77bdc20a1af76bf6c16c7
-
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-    
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
- final TextEditingController  emailController = TextEditingController();
- final TextEditingController  passwordController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   bool obscureText = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
         title: const Text('Login'),
       ),
       body: SingleChildScrollView(
@@ -50,42 +27,46 @@ class _LoginPageState extends State<LoginPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-            const  Padding(padding:  EdgeInsets.all(80)),
+              const Padding(padding: EdgeInsets.all(80)),
               InputItem(
-                controller: emailController, 
-                labelText: 'Email',
-                hintText: 'email', 
-                  icon: const Icon(Icons.email)
-                  ),
+                  controller: emailController,
+                  labelText: 'Email',
+                  hintText: 'email',
+                  icon: const Icon(Icons.email)),
               const SizedBox(height: 20.0),
               InputItem(
                 controller: passwordController,
-                 labelText: 'Password', 
-                 hintText: 'password',
-                 obscureText: obscureText,  
-                 icon: const Icon(Icons.lock), 
-                 suffixIcon: IconButton(icon: Icon(obscureText ? Icons.visibility: Icons.visibility_off),
-                  onPressed: () { 
+                labelText: 'Password',
+                hintText: 'password',
+                obscureText: obscureText,
+                icon: const Icon(Icons.lock),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                      obscureText ? Icons.visibility : Icons.visibility_off),
+                  onPressed: () {
                     // Cambia el estado de la contraseña
                     setState(() {
                       obscureText = !obscureText;
                     });
                   },
-                        ),
+                ),
               ),
               const SizedBox(height: 20.0),
               ElevatedButton(
                 onPressed: () {
                   // Verifica las credenciales ingresadas
-                  if ( emailController.text == 'joseph.alcerro@unah.hn'  && passwordController.text == '20222000391' ) {
+                  if (emailController.text == 'joseph.alcerro@unah.hn' &&
+                      passwordController.text == '20222000391') {
                     // Si las credenciales son correctas, navega a la página de inicio
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const InicioPage())
-                    );
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const InicioPage()));
                     return;
                   } else {
                     // Si las credenciales son incorrectas, muestra un mensaje de error
                     ScaffoldMessenger.of(context).showSnackBar(
-                    const  SnackBar( 
+                      const SnackBar(
                         content: Text('Credenciales incorrectas'),
                         backgroundColor: Colors.red,
                       ),
@@ -93,7 +74,8 @@ class _LoginPageState extends State<LoginPage> {
                   }
                 },
                 style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 30),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20),
                   ),
@@ -101,13 +83,14 @@ class _LoginPageState extends State<LoginPage> {
                 child: const Text(
                   'Login',
                   style: TextStyle(
-                      fontSize: 18, color: Colors.blueAccent), // Tamaño del texto
+                      fontSize: 18,
+                      color: Colors.blueAccent), // Tamaño del texto
                 ),
               ),
-               const SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
               ElevatedButton(
                 onPressed: () {
-                 Navigator.pushNamed(context, MyRoutes.registroRoute.name );
+                  Navigator.pushNamed(context, MyRoutes.registroRoute.name);
                 },
                 style: ElevatedButton.styleFrom(foregroundColor: Colors.white),
                 child: const Text(
@@ -117,95 +100,8 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ],
           ),
-=======
-        title: Text('Login'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'Email',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Verifica las credenciales ingresadas
-                String email = _emailController.text;
-                String password = _passwordController.text;
-
-                if (email == Email && password == Password) {
-                  // Si las credenciales son correctas, navega a la página de inicio
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          InicioPage(), // Reemplaza LoginPage con la página de inicio
-                    ),
-                  );
-                } else {
-                  // Si las credenciales son incorrectas, muestra un mensaje de error
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Credenciales incorrectas'),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              child: Text(
-                'Login',
-                style: TextStyle(
-                    fontSize: 18, color: Colors.blueAccent), // Tamaño del texto
-              ),
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RegistroPage(),
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(foregroundColor: Colors.white),
-              child: Text(
-                'Registrarse',
-                style: TextStyle(color: Colors.blueAccent), // Color del texto
-              ),
-            ),
-          ],
->>>>>>> 79b1c0a2b8fedfc06fe77bdc20a1af76bf6c16c7
         ),
       ),
     );
   }
 }
-
-<<<<<<< HEAD
-=======
-void main() {
-  runApp(MaterialApp(
-    home: LoginPage(),
-  ));
-}
->>>>>>> 79b1c0a2b8fedfc06fe77bdc20a1af76bf6c16c7
--- lib/pantallas/registro_page.dart
+++ lib/pantallas/registro_page.dart
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-
 import 'package:ap_autentication/widgets/Imput_widget.dart';
-=======
->>>>>>> 79b1c0a2b8fedfc06fe77bdc20a1af76bf6c16c7
 import 'package:flutter/material.dart';
 
 class RegistroPage extends StatelessWidget {
@@ -19,42 +15,12 @@ class RegistroPage extends StatelessWidget {
   final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text('Registro'),
-      ),
-      body:  SingleChildScrollView(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          key: formkey, //Llave para el formulario
-          child: Column(
-            children: [
-            const Icon(Icons.person, size: 100, color: Colors.blue,),
-            InputItem(
-              controller: controllerNombre,labelText: "Nombre",
-              hintText: "Escribe tu nombre",
-              ),
-            const Padding(padding:  EdgeInsets.all(10)),
-            InputItem(
-             controller: controllerCorreo,labelText:"Correo"
-            ,hintText: "Escribe tu correo electrónico",
-=======
     return Scaffold(
         appBar: AppBar(
-          title: const Center(
-              child: Text(
-            'Registro',
-            style: TextStyle(
-                color: Colors.blue,
-                fontSize: 20,
-                fontWeight: FontWeight.normal),
-          )),
+          title: const Text('Registro'),
         ),
         body: SingleChildScrollView(
-          padding: const EdgeInsets.all(20),
-          child: Card(
-            elevation: 10,
+            padding: const EdgeInsets.all(20),
             child: Form(
               key: formkey, //Llave para el formulario
               child: Column(
@@ -158,121 +124,6 @@ class RegistroPage extends StatelessWidget {
                   const Padding(padding: EdgeInsets.all(5)),
                 ],
               ),
->>>>>>> 79b1c0a2b8fedfc06fe77bdc20a1af76bf6c16c7
-            ),
-            const Padding(padding:  EdgeInsets.all(10)),
-            InputItem(controller: controllertelefono, labelText: "Telefono",
-            hintText: "Escribe tu telefono",),
-            const Padding(padding:  EdgeInsets.all(10)),
-            InputItem(
-            controller: controllerContrasenia, labelText: "Contraseña",
-            hintText: "Escribe tu contraseña",
-            ),
-            const Padding(padding:  EdgeInsets.all(10)),
-            InputItem(
-              controller: controllerContrasenia2, labelText: "Repetir contraseña",
-              hintText: "Confirma tu contraseña",
-            
-              ),
-              const Padding(padding:  EdgeInsets.all(10)),
-              ElevatedButton(
-                onPressed: (){
-        
-                  final nombre= controllerNombre.text;
-                  final correo= controllerCorreo.text;
-                  final telefeno = controllertelefono.text;
-                  final contrasenia= controllerContrasenia.text;
-                  final contrasenia2= controllerContrasenia2.text;
-                  
-        
-                  if(nombre.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("El nombre no puede estar vacío"))
-                    );
-                    return;
-                  }
-        
-                  if(correo.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("El correo no puede estar vacío"))
-                    );
-                    return;
-                  }
-        
-                 if(telefeno.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("El telefono no puede estar vacío"))
-                    );
-                    return;
-                  }
-                  if(contrasenia.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("La contraseña no puede estar vacía"))
-                    );
-                    return;
-                  }
-        
-                  if(contrasenia2.isEmpty){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("La contraseña no puede estar vacía"))
-                    );
-                    return;
-                  }
-                    if(contrasenia == contrasenia2){
-                    print("Nombre: ${controllerNombre.text}");
-                    print("Correo: ${controllerCorreo.text}");
-                    print("Contraseña: ${controllerContrasenia.text}");
-                    print("Contraseña2: ${controllerContrasenia2.text}");
-                    return;
-                    }
-                    else{
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Las contraseñas no coinciden"))
-                      );
-                      return;
-                    }
-                },
-                 child: const Text("    Registrate   "),
-                 ),
-              const Padding(padding:  EdgeInsets.all(5)),
-              ],
-          ),
-        ));
+            )));
   }
 }
-
-<<<<<<< HEAD
-=======
-class InputItem extends StatefulWidget {
-  const InputItem(
-      {super.key,
-      required this.controller,
-      this.labelText,
-      this.hintText,
-      this.validator});
-
-  final TextEditingController controller;
-  final String? labelText;
-  final String? hintText;
-
-  final String? Function(String?)? validator;
-
-  @override
-  State<InputItem> createState() => _InputItemState();
-}
-
-class _InputItemState extends State<InputItem> {
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      validator: widget.validator,
-      controller: widget.controller,
-      decoration: InputDecoration(
-        labelText: widget.labelText,
-        hintText: widget.hintText,
-        border: const OutlineInputBorder(),
-      ),
-    );
-  }
-}
->>>>>>> 79b1c0a2b8fedfc06fe77bdc20a1af76bf6c16c7
